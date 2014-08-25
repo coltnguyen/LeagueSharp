1c542fe3fc00ed6820f93bbf99f8184c2d77c7a3
GuiltyWolf <GuiltyWolf@users.noreply.github.com>
2014-08-25 17:00:55 -0600

    Update AssemblyInfo.cs

--- WolfZilean/Properties/AssemblyInfo.cs
+++ WolfZilean/Properties/AssemblyInfo.cs
@@ -1,15 +1,15 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WolfZilean")]
+[assembly: AssemblyTitle("Zilean")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WolfZilean")]
+[assembly: AssemblyProduct("Zilean")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
