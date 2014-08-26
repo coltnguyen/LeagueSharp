fb4914e9b3c5c91fb9a61ada1a2d450cce3eecef
GuiltyWolf <GuiltyWolf@users.noreply.github.com>
2014-08-25 23:37:04 -0600

    Update Program.cs

--- WolfZilean/Program.cs
+++ WolfZilean/Program.cs
@@ -1,4 +1,3 @@
-//Thanks iSnorflake for the template, PQMailer for some error fixing and FlapperDoodle for some info
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,13 +8,12 @@ using LeagueSharp;
 using LeagueSharp.Common;
 using SharpDX;
 using Color = System.Drawing.Color;
+
 namespace WolfZilean
 {
     class Program
     {
-        //Calls Champion
         public static string ChampName = "Zilean";
-        //Orbwalker
         public static Orbwalking.Orbwalker Orbwalker;
         public static Obj_AI_Base Player = ObjectManager.Player; // Instead of typing ObjectManager.Player you can just type Player
         //Spells
@@ -35,7 +33,7 @@ namespace WolfZilean
             Q = new Spell(SpellSlot.Q, 700);
             W = new Spell(SpellSlot.W);
             E = new Spell(SpellSlot.E, 700);
-            R = new Spell(SpellSlot.R, 900);
+            R = new Spell(SpellSlot.R);
 
             SpellList.Add(Q);
             SpellList.Add(W);
@@ -57,20 +55,11 @@ namespace WolfZilean
             Wolf.SubMenu("Combo").AddItem(new MenuItem("useW", "Use W").SetValue(true));
             Wolf.SubMenu("Combo").AddItem(new MenuItem("useE", "Use E").SetValue(true));
             Wolf.SubMenu("Combo").AddItem(new MenuItem("ComboActive", "Combo").SetValue(new KeyBind(32, KeyBindType.Press)));
-            //Ult menu
-            //Wolf.AddSubMenu(new Menu("Ult", "Ult Settings"));
-            //Wolf.SubMenu("Ult").AddItem(new MenuItem("UseR", "Use R").SetValue(true));
-            //Wolf.SubMenu("Ult").AddItem(new MenuItem("HealthPercent", "HP Trigger Percent").SetValue(new Slider(15)));
-            //Drawings menu:
+            //Drawings menu
             Wolf.AddSubMenu(new Menu("Drawings", "Drawings"));
             Wolf.SubMenu("Drawings").AddItem(new MenuItem("QRange", "Q/E").SetValue(new Circle(false, Color.FromArgb(100, 255, 0, 255))));
-            Wolf.SubMenu("Drawings").AddItem(new MenuItem("RRange", "R").SetValue(new Circle(false, Color.FromArgb(100, 255, 0, 255))));
-            //Exploits
-            //Wolf.AddItem(new MenuItem("NFE", "No-Face Exploit").SetValue(true));
             //Make the menu visible
             Wolf.AddToMainMenu();
-            
-
 
             Drawing.OnDraw += Drawing_OnDraw; // Add onDraw
             Game.OnGameUpdate += Game_OnGameUpdate; // adds OnGameUpdate (Same as onTick in bol)
@@ -96,32 +85,41 @@ namespace WolfZilean
 
             if (useQ && Q.IsReady())
             {
-                Q.CastOnUnit(target, true);
+                Q.CastOnUnit(target);
             }
+
             if (useW && W.IsReady())
             {
-                W.Cast();
+                if (Q.IsReady())
+                {
+                    return;
+                }
+                else
+                {
+                    if (useW && W.IsReady())
+                    {
+                        W.Cast();
+                    }
+                }
             }
+
             if (useE && E.IsReady())
             {
-                E.CastOnUnit(target, true);
+                E.CastOnUnit(target);
             }
         }
+
         public static void Drawing_OnDraw(EventArgs args)
         {
             var menuItem = Wolf.Item("QRange").GetValue<Circle>();
-            var menuItem2 = Wolf.Item("RRange").GetValue<Circle>();
             if (menuItem.Active) Utility.DrawCircle(Player.Position, Q.Range, menuItem.Color);
-            if (menuItem2.Active) Utility.DrawCircle(Player.Position, R.Range, menuItem.Color);
             //Draw Ranges of Abilities
             foreach (var spell in SpellList)
             {
                 menuItem = Wolf.Item(spell.Slot + "Range").GetValue<Circle>();
-                menuItem2 = Wolf.Item(spell.Slot + "Range").GetValue<Circle>();
-                if (menuItem.Active && menuItem2.Active) 
+                if (menuItem.Active)
                     Utility.DrawCircle(Player.Position, spell.Range, menuItem.Color);
+                }
             }
         }
-
     }
-}
