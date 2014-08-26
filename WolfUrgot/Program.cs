15f1fd582107c99573d9402f8947378da31ab669
GuiltyWolf <GuiltyWolf@users.noreply.github.com>
2014-08-26 14:37:37 -0600

    Update Program.cs

--- WolfUrgot/Program.cs
+++ WolfUrgot/Program.cs
@@ -1,31 +1,31 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using System.Drawing;
 using LeagueSharp;
 using LeagueSharp.Common;
-using SharpDX;
-using Color = System.Drawing.Color;
+
 namespace WolfUrgot
 {
-    class Program
+    internal class Program
     {
         public static string ChampName = "Urgot";
         public static Orbwalking.Orbwalker Orbwalker;
-        public static Obj_AI_Base Player = ObjectManager.Player; // Instead of typing ObjectManager.Player you can just type Player
+
+        public static Obj_AI_Base Player = ObjectManager.Player;
+            // Instead of typing ObjectManager.Player you can just type Player
+
         //Spells
         public static List<Spell> SpellList = new List<Spell>();
         public static Spell Q, Q2, W, E;
 
         public static Menu Wolf;
-        static void Main(string[] args)
+
+        private static void Main(string[] args)
         {
             CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
         }
 
-        static void Game_OnGameLoad(EventArgs args)
+        private static void Game_OnGameLoad(EventArgs args)
         {
             if (Player.BaseSkinName != ChampName) return;
 
@@ -34,7 +34,8 @@ namespace WolfUrgot
             W = new Spell(SpellSlot.W);
             E = new Spell(SpellSlot.E, 900);
 
-            Q.SetSkillshot(0.10f, 100f, 1600f, true, SkillshotType.SkillshotLine); //Skillshot Delay, Width, Speed, Collision (T/F), Prediction (Line/Circle/Cone)
+            Q.SetSkillshot(0.10f, 100f, 1600f, true, SkillshotType.SkillshotLine);
+                //Skillshot Delay, Width, Speed, Collision (T/F), Prediction (Line/Circle/Cone)
             Q2.SetSkillshot(0.10f, 100f, 1600f, false, SkillshotType.SkillshotLine);
             E.SetSkillshot(0.283f, 0f, 1750f, false, SkillshotType.SkillshotCircle);
 
@@ -56,11 +57,14 @@ namespace WolfUrgot
             Wolf.AddSubMenu(new Menu("Combo", "Combo"));
             Wolf.SubMenu("Combo").AddItem(new MenuItem("useQ", "Use Q").SetValue(true));
             Wolf.SubMenu("Combo").AddItem(new MenuItem("useE", "Use E").SetValue(true));
-            Wolf.SubMenu("Combo").AddItem(new MenuItem("ComboActive", "Combo").SetValue(new KeyBind(32, KeyBindType.Press)));
-             //Drawings menu:
+            Wolf.SubMenu("Combo")
+                .AddItem(new MenuItem("ComboActive", "Combo").SetValue(new KeyBind(32, KeyBindType.Press)));
+            //Drawings menu:
             Wolf.AddSubMenu(new Menu("Drawings", "Drawings"));
-            Wolf.SubMenu("Drawings").AddItem(new MenuItem("QRange", "Q").SetValue(new Circle(false, Color.FromArgb(100, 255, 0, 255))));
-            Wolf.SubMenu("Drawings").AddItem(new MenuItem("ERange", "E").SetValue(new Circle(false, Color.FromArgb(100, 255, 0, 255))));
+            Wolf.SubMenu("Drawings")
+                .AddItem(new MenuItem("QRange", "Q").SetValue(new Circle(false, Color.FromArgb(100, 255, 0, 255))));
+            Wolf.SubMenu("Drawings")
+                .AddItem(new MenuItem("ERange", "E").SetValue(new Circle(false, Color.FromArgb(100, 255, 0, 255))));
             //Exploits
             //Wolf.AddItem(new MenuItem("NFE", "No-Face Exploit").SetValue(true));
             //Make the menu visible
@@ -72,7 +76,7 @@ namespace WolfUrgot
             Game.PrintChat("Wolf" + ChampName + " loaded! By GuiltyWolf");
         }
 
-        static void Game_OnGameUpdate(EventArgs args)
+        private static void Game_OnGameUpdate(EventArgs args)
         {
             if (Wolf.Item("ComboActive").GetValue<KeyBind>().Active)
             {
@@ -89,34 +93,35 @@ namespace WolfUrgot
         {
             var useQ = Wolf.Item("useQ").GetValue<bool>();
             var useE = Wolf.Item("useE").GetValue<bool>();
-            var target = SimpleTs.GetTarget(Q.Range, SimpleTs.DamageType.Physical);
+            Obj_AI_Hero target = SimpleTs.GetTarget(Q.Range, SimpleTs.DamageType.Physical);
             if (target == null) return;
 
             if (useQ && Q.IsReady() && target.HasBuff("UrgotCorrosiveDebuff"))
             {
-                    Q2.Cast(target, true);
+                Q2.Cast(target);
             }
             else
             {
                 if (useQ && Q.IsReady())
                 {
-                        Q.CastIfHitchanceEquals(target, HitChance.Medium);
-                    }
+                    Q.CastIfHitchanceEquals(target, HitChance.Medium);
                 }
-                if (W.IsReady() && target.HasBuff("UrgotCorrosiveDebuff"))
-                {
-                    W.Cast();
-                }
-
-                if (useE && E.IsReady())
+            }
+            if (W.IsReady() && target.HasBuff("UrgotCorrosiveDebuff"))
             {
-                var eTarget = SimpleTs.GetTarget(E.Range, SimpleTs.DamageType.Physical);
+                W.Cast();
+            }
+
+            if (useE && E.IsReady())
+            {
+                Obj_AI_Hero eTarget = SimpleTs.GetTarget(E.Range, SimpleTs.DamageType.Physical);
                 if (E.IsReady() && eTarget.IsValidTarget())
                 {
                     E.CastIfHitchanceEquals(target, HitChance.High);
                 }
             }
         }
+
         public static void Drawing_OnDraw(EventArgs args)
         {
             var menuItem = Wolf.Item("QRange").GetValue<Circle>();
@@ -124,7 +129,7 @@ namespace WolfUrgot
             if (menuItem.Active) Utility.DrawCircle(Player.Position, Q.Range, menuItem.Color);
             if (menuItem2.Active) Utility.DrawCircle(Player.Position, E.Range, menuItem.Color);
             //Draw Ranges of Abilities
-            foreach (var spell in SpellList)
+            foreach (Spell spell in SpellList)
             {
                 menuItem = Wolf.Item(spell.Slot + "Range").GetValue<Circle>();
                 menuItem2 = Wolf.Item(spell.Slot + "Range").GetValue<Circle>();
